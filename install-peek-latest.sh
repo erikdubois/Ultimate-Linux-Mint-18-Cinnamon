963aa81be437156d8212495546a239e6be48b823
Erik Dubois <erik.dubois@gmail.com>
2016-09-25 20:50:45 +0200

    Commit comment :  on Sun Sep 25 20:50:45 CEST 2016

--- install-peek-latest.sh
+++ install-peek-latest.sh
@@ -57,6 +57,7 @@
 # Latest builds of cinnamon and nemo
 
 wget https://github.com/phw/peek/releases/download/v0.7.2/peek-0.7.2-Linux.deb
+sudo apt install libsdl1.2debian ffmpeg libavdevice-ffmpeg56 -y
 sudo dpkg -i peek-0.7.2-Linux.deb
 rm peek-0.7.2-Linux.deb
 
