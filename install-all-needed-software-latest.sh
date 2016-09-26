48d8fdaf32670a655f4cb853f85612e629ecdce0
Erik Dubois <erik.dubois@gmail.com>
2016-09-26 11:14:35 +0200

    Commit comment :  on Mon Sep 26 11:14:35 CEST 2016

--- install-all-needed-software-latest.sh
+++ install-all-needed-software-latest.sh
@@ -108,7 +108,7 @@ rm /tmp/sublime-text_build-3126_amd64.deb
 wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O /tmp/sublime-text_build-3126_amd64.deb
 sudo dpkg -i /tmp/sublime-text_build-3126_amd64.deb
 
-rm /tmp/sublime-text_build-3126_amd64.de
+rm /tmp/sublime-text_build-3126_amd64.deb
 
 
 ###############################################################################################
