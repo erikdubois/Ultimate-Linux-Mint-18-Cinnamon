b866f82fed77a5b7d65a0d6dfa9dfbfed4fea513
Erik Dubois <erik.dubois@gmail.com>
2016-09-26 16:46:35 +0200

    Commit comment :  on Mon Sep 26 16:46:35 CEST 2016

--- install-gtk-vertex-theme.sh
+++ install-gtk-vertex-theme.sh
@@ -54,12 +54,28 @@
 #
 ##################################################################################################################
 
-#https://github.com/horst3180/vertex-theme
+# https://github.com/horst3180/vertex-theme
 
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/vertex-theme.list"
-sudo apt-get update
-sudo apt-get install vertex-theme
+rm -rf /tmp/vertex-theme
 
+sudo rm -rf /usr/share/themes/{Vertex,Vertex-Dark,Vertex-Light,Vertex-Gnome-Shell,Vertex-Gnome-Shell-3.16,Vertex-Cinnamon}
+rm -rf ~/.local/share/themes/{Vertex,Vertex-Dark,Vertex-Light,Vertex-Gnome-Shell,Vertex-Gnome-Shell-3.16,Vertex-Cinnamon}
+rm -rf ~/.themes/{Vertex,Vertex-Dark,Vertex-Light,Vertex-Gnome-Shell,Vertex-Gnome-Shell-3.16,Vertex-Cinnamon}
+
+sudo apt-get install -y autoconf automake pkg-config libgtk-3-dev git
+
+git clone https://github.com/horst3180/vertex-theme --depth 1  /tmp/vertex-theme
+cd /tmp/vertex-theme
+./autogen.sh --prefix=/usr
+sudo make install
+
+rm -rf /tmp/vertex-theme
+
+
+
+#sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/vertex-theme.list"
+#sudo apt-get update
+#sudo apt-get install vertex-theme -y
 
 
 
