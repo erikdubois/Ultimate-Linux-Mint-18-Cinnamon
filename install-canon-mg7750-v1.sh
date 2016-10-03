6ec07267c1e052f41e25b3c9afb9e6f99c69e660
Erik Dubois <erik.dubois@gmail.com>
2016-10-03 21:16:12 +0200

    Commit comment :  on Mon Oct  3 21:16:12 CEST 2016

--- install-canon-mg7750-v1.sh
+++ install-canon-mg7750-v1.sh
@@ -19,17 +19,19 @@
 #sudo apt-get install libxml2 libglade2-0 libpng3 libtiff5 -y
 #cd cnijfilter2-5.20-1-deb
 #./install.sh
-cd canon
+cd canon-pixma-mg7750
 tar -xzvf cnijfilter2-5.20-1-deb.tar.gz
 tar -xzvf scangearmp2-3.20-1-deb.tar.gz
 
 sudo dpkg -i cnijfilter2-5.20-1-deb/packages/cnijfilter2_5.20-1_amd64.deb
 sudo dpkg -i scangearmp2-3.20-1-deb/packages/scangearmp2_3.20-1_amd64.deb
 
+
 # deleting the uncompressed folder to keep a clean github
-echo "Extracted folders have been deleted"
 rm -r cnijfilter2-5.20-1-deb
 rm -r scangearmp2-3.20-1-deb
+
+echo "Extracted folders have been deleted"
 
 ############################################################################
 
