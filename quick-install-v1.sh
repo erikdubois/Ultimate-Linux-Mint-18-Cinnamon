d5c4caddc9c112615becdd1f439fc0e022bcea91
Erik Dubois <erik.dubois@gmail.com>
2016-10-19 16:42:00 +0200

    Comment :  on 16:42:00 19-10-2016

--- quick-install-v1.sh
+++ quick-install-v1.sh
@@ -61,7 +61,7 @@ echo "###################    T H E   E N D      ######################"
 echo "################################################################"
 
 
-sh update-to-the-last-stable-4.8-1-2.sh
+sh update-to-the-last-stable-4.8-2.sh
 
 echo
 echo
