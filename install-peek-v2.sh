164a28697ddbfff95ec683b81499951078c8b4cf
Erik Dubois <erik.dubois@gmail.com>
2016-10-22 14:12:41 +0200

    Comment :  on 14:12:41 22-10-2016

--- install-peek-v2.sh
+++ install-peek-v2.sh
@@ -16,6 +16,7 @@
 
 wget https://github.com/phw/peek/releases/download/v0.7.2/peek-0.7.2-Linux.deb
 sudo apt install libsdl1.2debian ffmpeg libavdevice-ffmpeg56 -y
+sudo apt-get -f install -y
 sudo dpkg -i peek-0.7.2-Linux.deb
 rm peek-0.7.2-Linux.deb
 
