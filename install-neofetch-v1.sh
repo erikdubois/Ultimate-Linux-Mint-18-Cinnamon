d2b70ee97b66de7810864d2310c24a248d96c0ef
Erik Dubois <erik.dubois@gmail.com>
2016-11-05 21:51:10 +0100

    Comment :  on 21:51:10 05-11-2016

--- install-neofetch-v1.sh
+++ install-neofetch-v1.sh
@@ -14,15 +14,14 @@
 
 
 # repo for spotify
-sudo add-apt-repository "deb http://repository.spotify.com stable non-free" -y
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886
 
+sudo add-apt-repository -y ppa:dawidd0811/neofetch
 
 # getting new info of this new repo
 sudo apt-get -y update
 
 # installing
-sudo apt-get install spotify-client -y
+sudo apt install neofetch -y
 
 echo "################################################################"
 echo "###################    T H E   E N D      ######################"
