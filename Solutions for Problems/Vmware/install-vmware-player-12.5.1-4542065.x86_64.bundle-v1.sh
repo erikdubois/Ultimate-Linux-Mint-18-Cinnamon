ce3ce6b559930c0dd79285c46e19ac8078b19613
Erik Dubois <erik.dubois@gmail.com>
2016-11-12 14:23:30 +0100

    Comment :  on 14:23:30 12-11-2016

---
+++ Solutions for Problems/Vmware/install-vmware-player-12.5.1-4542065.x86_64.bundle-v1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+
+rm /tmp/vmware-player.bundle
+
+wget http://www.vmware.com/go/tryplayerpro-linux-64 -O /tmp/vmware-player.bundle
+
+chmod +x /tmp/vmware-player.bundle
+
+sudo /tmp/vmware-player.bundle
+
+rm /tmp/vmware-player.bundle
+
+# install gcc6 needed to run
+# http://askubuntu.com/questions/822207/how-do-i-install-gcc-5-0-on-ubuntu-or-linux-mint
+sudo add-apt-repository  -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-6 -y
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
+
+
+##################################################################################################################
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
+
+
