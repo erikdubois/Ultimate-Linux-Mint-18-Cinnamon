390dd45c6d88a09a182b1989b60ed4a1890d5374
Erik Dubois <erik.dubois@gmail.com>
2016-12-12 07:26:04 +0100

    Comment :  on 07:26:04 12-12-2016

---
+++ update-to-the-last-stable-4.9-0-v1.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+#
+#                                       
+# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
+# MMm----::-://////////////oymNMd+`     
+# MMd      /++                -sNMd:    
+# MMNso/`  dMM    `.::-. .-::.` .hMN:   
+# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
+#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  .mmd  `mmm   yMM. dMM   
+#     NMm  dMM`  ..`   ...   ydm. dMM   
+#     hMM- +MMd/-------...-:sdds  dMM   
+#     -NMm- :hNMNNNmdddddddddy/`  dMM   
+#      -dMNs-``-::::-------.``    dMM   
+#       `/dMNmy+/:-------------:/yMMM  
+#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
+#             \.MMMMMMMMMMMMMMMMMMM    
+#                                      
+#
+#
+##################################################################################################################
+#
+# Current project : Ultimate-Linux-Mint-18
+#
+# Source 	: 	https://github.com/erikdubois/Ultimate-Linux-Mint-18
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+# 
+# More from Erik Dubois
+#
+# Aurora Conky
+# at http://sourceforge.net/projects/auroraconkytheme/
+# Explanation on the use of this theme can be found at 
+# http://erikdubois.be/category/linux/aurora-conky/
+# 
+# Aureola Conky
+# Collections of nice conky's with lua syntax
+# https://github.com/erikdubois/Aureola
+#
+# Sardi icons
+# Many different styles of icons from colourfull, monochrome, white, circle
+# https://sourceforge.net/projects/sardi/
+#
+# Super Ultra Flat Numix Remix
+# Colourfull and playfull icons
+# https://github.com/erikdubois/Super-Ultra-Flat-Numix-Remix
+#
+# Check out the github - many more scripts for automatic installation of Linux Systems.
+#
+#
+#
+#
+#
+##################################################################################################################
+# If the option -y has been added. It will autoinstall all. Omit if you do not want that.
+##################################################################################################################
+#
+#
+#
+#
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+#  H I G H L Y    E X P E R I M E N T A L
+
+#  Better to read and do a step by step upgrade 
+
+#  http://erikdubois.be/linux/the-ultimate-linux-mint-update
+
+#  if you want to experiment on a computer where there is no data loss risk
+
+#  please be my guest
+
+echo "Let us check if your computer is up-to-date"
+
+
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
+#sudo apt-get autoclean -y
+#sudo apt-get autoremove -y
+
+#Grub customizer if you dual boot
+#sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
+#sudo apt-get -y update
+#sudo apt-get install -y grub-customizer
+
+
+# latest kernel check the following url
+# http://kernel.ubuntu.com/~kernel-ppa/mainline/
+# at the moment you can have kernel 4.4.0
+# depending of you hardware you can install it as wel
+# keep in mind that nvidia or ati drivers sometimes clash with the kernel
+# read the latest article on these matters at http://erikdubois.be
+
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9/linux-headers-4.9.0-040900_4.9.0-040900.201612111631_all.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9/linux-headers-4.9.0-040900-generic_4.9.0-040900.201612111631_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9/linux-image-4.9.0-040900-generic_4.9.0-040900.201612111631_amd64.deb
+
+sudo dpkg -i linux*
+
+rm linux-*
+
+
+#ending
+#mkdir $HOME/Upload
+#sudo apt-get -y update
+#sudo apt-get -f -y install
+#sudo apt-get -y upgrade
+
+#sudo apt-get -y autoremove
+#sudo apt-get -y autoclean
+
+
+echo "################################################################"
+echo "###############      Kernel is installed            ############"
+echo "###############     Files have been deleted         ############"
+echo "################################################################"
+
+
+
+
+
+
+
